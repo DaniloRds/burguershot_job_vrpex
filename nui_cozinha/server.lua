1832c134f6a206114d014223512503f711139edf
Danilo <77410497+DaniloRds@users.noreply.github.com>
2025-04-13 22:45:43 -0300

    Atualizado

--- nui_cozinha/server.lua
+++ nui_cozinha/server.lua
@@ -5,7 +5,6 @@ local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP")
-local inventory = module("vrp","cfg/inventory")
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CONEXÃO
